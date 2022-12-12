165b06a804a01913f41e4216d569da55fe93582a
Huang Rui <vowstar@gmail.com>
2022-12-12 15:31:58 +0800

    */*: bump version to 1.2022.14
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- scripts/get-plantuml-jar.js
+++ scripts/get-plantuml-jar.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 'use strict'
 
-var fs = require('fs')
-var path = require('path')
+const fs = require('fs')
+const path = require('path')
 
-var download = require('./download')
+const download = require('./download')
 
-var PACKAGE_JSON_PATH = path.join(__dirname, '../package.json')
-var plantumlVersion = require(PACKAGE_JSON_PATH).plantumlVersion
+const PACKAGE_JSON_PATH = path.join(__dirname, '../package.json')
+const plantumlVersion = require(PACKAGE_JSON_PATH).plantumlVersion
 
-var JAR_DIR_PATH = path.join(__dirname, '../vendor')
-var PLANTUML_JAR = path.join(JAR_DIR_PATH, 'plantuml.jar')
-var PLANTUML_JAR_VER_FILE = path.join(JAR_DIR_PATH, plantumlVersion)
+const JAR_DIR_PATH = path.join(__dirname, '../vendor')
+const PLANTUML_JAR = path.join(JAR_DIR_PATH, 'plantuml.jar')
+const PLANTUML_JAR_VER_FILE = path.join(JAR_DIR_PATH, plantumlVersion)
 
-var PLANTUML_FILES_URL = 'https://sourceforge.net/projects/plantuml/files/'
-var PLANTUML_FILES_JAR_PATH = plantumlVersion + '/plantuml.' + plantumlVersion + '.jar'
+const PLANTUML_FILES_URL = 'https://sourceforge.net/projects/plantuml/files/'
+const PLANTUML_FILES_JAR_PATH = plantumlVersion + '/plantuml.' + plantumlVersion + '.jar'
 
 if (!fs.existsSync(JAR_DIR_PATH)) {
   fs.mkdirSync(JAR_DIR_PATH)
@@ -24,7 +24,7 @@ if (!fs.existsSync(PLANTUML_JAR_VER_FILE) || !fs.existsSync(PLANTUML_JAR)) {
   console.log('Downloading plantuml.jar version ' + plantumlVersion)
   download(PLANTUML_FILES_URL + PLANTUML_FILES_JAR_PATH, PLANTUML_JAR, true)
   try {
-    var time = new Date()
+    const time = new Date()
     fs.utimesSync(PLANTUML_JAR_VER_FILE, time, time)
   } catch (err) {
     fs.closeSync(fs.openSync(PLANTUML_JAR_VER_FILE, 'w'))
